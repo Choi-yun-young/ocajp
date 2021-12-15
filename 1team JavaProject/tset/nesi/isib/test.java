5bebce4af8004aa25ff5ec27bdcd0ae63b1dba9f
JJU <JJU@DESKTOP-8BBDVH4>
2021-12-15 16:18:45 +0900

    개인자바에서 작성 후 싱크로하고 팀 싱크로에서 변경값 확인후 다시 개인 자바에서 커밋진행

---
+++ 1team JavaProject/tset/nesi/isib/test.java
@@ -0,0 +1,12 @@
+package tset.nesi.isib;
+
+public class test {
+	
+	
+	public void test11() {
+		
+		System.out.println("테스트 출력");
+		System.out.println("개인브런치");
+		System.out.println("4시20분");
+	}
+}
