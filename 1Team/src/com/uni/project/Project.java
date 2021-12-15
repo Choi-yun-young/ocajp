7a31f7a1c63b70f2e317ac992f5c32b6f0f83b82
gimmary <pukaintherain@gmail.com>
2021-12-15 16:52:04 +0900

    삭제

--- 1Team/src/com/uni/project/Project.java
+++ 1Team/src/com/uni/project/Project.java
@@ -1,34 +1,22 @@
 package com.uni.project;
 
 public class Project {
-	
-	
-	
-	
+
 	private static void method1() {
-		
-	
-		
-		
+
 		String str = "abc";
-		
+
 		System.out.println(str);
-		
 
 		String str1 = "abcd";
-		
+
 		System.out.println(str1);
 
 		String str2 = "abcde";
-		
+
 		System.out.println(str2);
-		
 
 		System.out.println("테스트");
-		
-		System.out.println("박주연");
 
+	}
 }
-}
-
-
