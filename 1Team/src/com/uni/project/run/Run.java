80cadcfc6d4a99066ce9c47e752007b1d4b75618
Choi-yun-young <96040336+Choi-yun-young@users.noreply.github.com>
2021-12-20 17:43:58 +0900

    다시등록

--- 1Team/src/com/uni/project/run/Run.java
+++ 1Team/src/com/uni/project/run/Run.java
@@ -1,10 +1,15 @@
 package com.uni.project.run;
 
+import com.uni.project.view.Customer;
+
 public class Run {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		Customer c= new Customer();
+		c.Main();
 	}
 
 }
