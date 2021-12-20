aa44f43098a199399e46c6a2f4c2c780a1c65ef2
Choi-yun-young <96040336+Choi-yun-young@users.noreply.github.com>
2021-12-20 16:57:44 +0900

    오류수정

--- 1Team/src/com/uni/project/run/Run.java
+++ 1Team/src/com/uni/project/run/Run.java
@@ -2,28 +2,4 @@ package com.uni.project.run;
 
 public class Run {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-
-		//Customer c= new Customer();
-		//c.Main();
-
-		//CakeyMenu c = new CakeyMenu();
-		//c.searchMenu();
-
-		//Customer c= new Customer();
-		//c.Main();
-
-//		ShopManager c = new ShopManager();
-//		c.cakeAllList();
-		
-//		SearchDao s = new SearchDao();
-//		s.getCake();
-		
-		//CakeyShopMenu c = new CakeyShopMenu();
-		//c.shopIncakeOrder();
-	}
-
 }
-
